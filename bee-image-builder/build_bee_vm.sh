8a7689a1d8340d9b377e84e8d10be13fcc5125cf
Louis Vernon <louis.vernon@gmail.com>
2017-07-21 13:18:16 -0600

    initial travis file

--- bee-image-builder/build_bee_vm.sh
+++ bee-image-builder/build_bee_vm.sh
@@ -5,6 +5,6 @@ chmod 600 ~/.bee/ssh_key/id_rsa
 # Build and add base image
 cp ubuntu1404-qemu-template.json ubuntu1404-qemu.json
 sed -i s/UUU/$USER/ ubuntu1404-qemu.json 
-packer build -var-file=ubuntu1404-qemu.json ubuntu-qemu.json
+packer build -var-file=ubuntu1404-qemu.json -var http_proxy=$http_proxy -var https_proxy=$https_proxy ubuntu-qemu.json
 
 
